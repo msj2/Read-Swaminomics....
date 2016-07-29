66e24f3935139b7f8490afb5dd983169a5c4a970
Mallikarjuna S J <msj2@users.noreply.github.com>
2016-07-30 03:08:21 +0530

    Update read_toi.py

--- read_toi.py
+++ read_toi.py
@@ -11,46 +11,15 @@ start=datetime.now()
 #full_url = raw_input("URL enter maadi: ")
 full_url = argv[1]
 #full_url = "http://blogs.timesofindia.indiatimes.com/Swaminomics/do-not-misuse-lokpal-act-to-harass-ngos/"
-lul = full_url
-print lul
+
 content = lxml.html.parse(full_url)
 
-# Doesn't work: Is div class = content under div id = sform2...... Try///
-# Prints empty..
-#lines = content.xpath("//div/@sform2")
 
-lines = content.xpath("//p/text()")		# Sexy Works.. But gets only Text, not the URL...
-#lines = content.xpath("//a/@href") # Works
-#lines = content.xpath("//img/@src") # Works
+lines = content.xpath("//p/text()")		#  Works.. But gets only Text, not the URL.
 
-
-
-##import lxml.html
-
-##htmltree = lxml.html.parse('http://www.google.com/intl/en/about/corporate/index.html')
-
-##p_tags = htmltree.xpath('//p')
-##p_content = [p.text_content() for p in p_tags]
-
-##print p_content
-
-
-
-#http://stackoverflow.com/questions/8226490/finding-html-element-with-class-using-lxml
-#	Doesn't work, Prints the below
-#lines = content.xpath("//div[contains(@class, 'content')]")
-#<Element div at 0x7fdb9c8b83c0>
-#<Element div at 0x7fdb9c8b8418>
-#<Element div at 0x7fdb9c8b8470>
-#<Element div at 0x7fdb9c8b84c8>
-
-
-#Doesn't work, Prints the below
-#<Element div at 0x7f863aa8c3c0>
-#lines = content.xpath("//div[@class='content']")
 for each in lines:
 	print each
 
 end=datetime.now()
 time_taken = end - start
-print "Time taken = ", time_taken
+print "Time taken = ", time_taken, "Now, you gotta read it within 5 min & take Altraday tablets"
